7d4e8da88178844ecabe73dcb83d651fbdfcce0a
LuckieLuke <luk19984@gmail.com>
2021-03-20 17:57:27 +0100

    configuration

--- back/app.py
+++ back/app.py
@@ -1,23 +1,22 @@
 from flask import Flask
+import time
 
 app = Flask(__name__)
 
-firebase_config = {
-    'apiKey': 'AIzaSyBEbg5wuvLNLWR2GG4uiWAE1AuyPlHl59c',
-    'authDomain': 'lettucemeat-c36a4.firebaseapp.com',
-    'projectId': 'lettucemeat-c36a4',
-    'storageBucket': 'lettucemeat-c36a4.appspot.com',
-    'messagingSenderId': '658041930488',
-    'appId': '1:658041930488:web:ec8928a3865fb7064c710d',
-    'measurementId': 'G-X9VBYG40BN'
 
-}
+@app.after_request
+def after_request(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
 
 
 @app.route('/')
 def main():
-    return 'hejka'
+    time.sleep(2)
+    return {'msg': 'hejka'}
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- front/lettuce-meat/src/App.js
+++ front/lettuce-meat/src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import { useState } from 'react';
+import { CircularProgress } from '@material-ui/core';
 
 function App() {
+  const [info, setInfo] = useState('Coś ciekawego')
+  const [waiting, setWaiting] = useState(false)
+
+
+  let getData = () => {
+    setWaiting(true)
+    fetch('http://localhost:5000')
+      .then(resp => resp.json())
+      .then(response => {
+        setWaiting(false)
+        setInfo(response.msg)
+      })
+      .catch(error => {
+        setInfo('PROBLEM')
+      })
+  }
+
   return (
     <div className="App">
-      <h1>cos tu bedzie</h1>
+      <h1>{waiting ? <CircularProgress color='secondary' /> : info}</h1>
+      <button onClick={getData}>Get sth from fetch</button>
     </div>
   );
 }
